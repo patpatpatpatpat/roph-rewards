6c83aace10afaab99654b64e3a7c7b4b49e1c227
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2017-10-27 12:11:54 +0800

    Add multi-account support

--- credentials.py
+++ credentials.py
@@ -1,2 +1,11 @@
-USERNAME = 'yourusername'
-PASSWORD = 'yourpassword'
+# Each account username/password combo must be placed inside a dict.
+CREDS_LIST = [
+    {
+        'USERNAME': 'myusername1',
+        'PASSWORD': 'mypassword1'
+    },
+    {
+        'USERNAME': 'myusername2',
+        'PASSWORD': 'mypassword2'
+    }
+]
